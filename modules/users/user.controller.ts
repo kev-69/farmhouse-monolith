0d7ea23652bfd6c7e2f9ed933634fffcd9748af2
kev-69 <bismarkobuobi19@gmail.com>
2025-04-24 08:24:33 +0000

    Implement user controller with getUser, updateUser, and deleteUser methods

--- modules/users/user.controller.ts
+++ modules/users/user.controller.ts
@@ -1,1 +1,51 @@
-// Handles routes: create user, get user, etc.
+import { Request, Response } from 'express';
+import { userService } from './user.service';
+
+export const userController = {
+    getUser: async (req: Request, res: Response) => {
+        try {
+            const user = await userService.getUser(req.user.id);
+            res.status(200).json({
+                message: "User profile retrieved successfully",
+                data: user
+            });
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).json({ message: error.message });
+            } else {
+                res.status(400).json({ message: 'An unknown error occurred' });
+            }
+        }
+    },
+
+    updateUser: async (req: Request, res: Response) => {
+        try {
+            const user = await userService.updateUser(req.user.id, req.body);
+            res.status(200).json({
+                meesage: "Profile updated successfully",
+                data: user
+            });
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).json({ message: error.message });
+            } else {
+                res.status(400).json({ message: 'An unknown error occurred' });
+            }
+        }
+    },
+
+    deleteUser: async (req: Request, res: Response) => {
+        try {
+            await userService.deleteUser(req.user.id);
+            res.status(200).json({
+                message: "Profile deleted successfully"
+            });
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).json({ message: error.message });
+            } else {
+                res.status(400).json({ message: 'An unknown error occurred' });
+            }
+        }
+    },
+};
