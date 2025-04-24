2754e986834fef7b82454db2341f281e77cecd4a
kev-69 <bismarkobuobi19@gmail.com>
2025-04-24 08:24:57 +0000

    Implement user module with routes for profile management

--- modules/users/user.module.ts
+++ modules/users/user.module.ts
@@ -1,1 +1,13 @@
 // Combines controller + service into a unit
+import { Router } from 'express';
+import { userController } from './user.controller';
+import validateToken from '../../middlewares/user.middleware';
+
+const router = Router();
+
+// Define routes
+router.get('/profile', validateToken, userController.getUser);
+router.put('/update-profile', validateToken, userController.updateUser);
+router.delete('/delete-profile', validateToken, userController.deleteUser);
+
+export const userModule = router;
