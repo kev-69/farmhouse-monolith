97e551eb098f73b63e59deb20b400481a099f2bf
kev-69 <bismarkobuobi19@gmail.com>
2025-04-25 08:23:15 +0000

    fix: correct user ID reference in user controller methods

--- modules/users/user.controller.ts
+++ modules/users/user.controller.ts
@@ -4,7 +4,7 @@ import { userService } from './user.service';
 export const userController = {
     getUser: async (req: Request, res: Response) => {
         try {
-            const user = await userService.getUser(req.user.id);
+            const user = await userService.getUser(req.user.userId);
             res.status(200).json({
                 message: "User profile retrieved successfully",
                 data: user
@@ -20,7 +20,7 @@ export const userController = {
 
     updateUser: async (req: Request, res: Response) => {
         try {
-            const user = await userService.updateUser(req.user.id, req.body);
+            const user = await userService.updateUser(req.user.userId, req.body);
             res.status(200).json({
                 meesage: "Profile updated successfully",
                 data: user
@@ -36,7 +36,7 @@ export const userController = {
 
     deleteUser: async (req: Request, res: Response) => {
         try {
-            await userService.deleteUser(req.user.id);
+            await userService.deleteUser(req.user.userId);
             res.status(200).json({
                 message: "Profile deleted successfully"
             });
