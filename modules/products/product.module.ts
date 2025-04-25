b851e3e264514fe63db662860c9d76cdd2db0928
kev-69 <bismarkobuobi19@gmail.com>
2025-04-25 08:20:37 +0000

    feat: implement product module with CRUD routes and validation middleware

--- modules/products/product.module.ts
+++ modules/products/product.module.ts
@@ -1,1 +1,37 @@
-// Combines product controller/service
+import { Router } from 'express';
+import { productController } from './product.controller';
+
+// middlewares to validate product data
+import { validate } from '../../middlewares/validation.middleware';
+import { createProductSchema, updateProductSchema } from './product.schema';
+
+// middlewares to authorize shops to add products, update products they own, and delete products they own
+import { validateToken, verifyShop, verifyProductOwnership } from '../../middlewares/shop.middleware';
+
+const router = Router();
+
+// Define routes
+router.post('/add-product', 
+    validateToken, 
+    verifyShop,
+    validate({ body: createProductSchema }), // Validate product data
+    productController.createProduct
+);
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProduct);
+
+router.put('/update-product/:id', 
+    validateToken,
+    verifyShop,
+    verifyProductOwnership, // Verify ownership of the product
+    validate({ body: updateProductSchema }), // Validate product data
+    productController.updateProduct
+);
+router.delete('/delete-product/:id',
+    validateToken,
+    verifyShop,
+    verifyProductOwnership, // Verify ownership of the product 
+    productController.deleteProduct
+);
+
+export const productModule = router;
