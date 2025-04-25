ea4399e30ce101c365e92cb8ad03cddaaf889ab6
kev-69 <bismarkobuobi19@gmail.com>
2025-04-25 08:17:11 +0000

    feat: implement category module with routes for adding, retrieving, updating, and deleting categories

--- modules/categories/category.module.ts
+++ modules/categories/category.module.ts
@@ -1,1 +1,17 @@
 // Combines category controller/services into a single module
+import { Router } from 'express';
+import { categoryController } from './category.controller';
+
+// middlewares to authorize shops to add categories, update categories they own, and delele categories they own
+import { validateToken, verifyShop, verifyCategoryOwnership } from '../../middlewares/shop.middleware';
+
+const router = Router();
+
+// Define routes
+router.post('/add-category', validateToken, verifyShop, categoryController.createCategory);
+router.get('/all-categories', categoryController.getAllCategories);
+router.get('/category/:id', categoryController.getCategory);
+router.put('/update-category/:id', validateToken, verifyCategoryOwnership, categoryController.updateCategory);
+router.delete('/delete-category/:id', validateToken, verifyCategoryOwnership, categoryController.deleteCategory);
+
+export const categoryModule = router;
