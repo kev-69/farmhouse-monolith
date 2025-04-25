998ccd1456a2531e24ac52c199e84315583e8e8c
kev-69 <bismarkobuobi19@gmail.com>
2025-04-25 08:20:15 +0000

    feat: implement product controller with CRUD operations and error handling

--- modules/products/product.controller.ts
+++ modules/products/product.controller.ts
@@ -1,1 +1,98 @@
 // Handles CRUD routes for products
+
+import { Request, Response } from 'express';
+import { productService } from './product.service';
+// Extend the Request interface to include the 'shop' property
+interface CustomRequest extends Request {
+    user?: { 
+        id: string 
+        shopId: string
+        role: string
+    };
+}
+
+
+export const productController = {
+    createProduct: async (req: CustomRequest, res: Response) => {
+        try {
+            // Include shopId from authenticated shop
+            const productData = {
+                ...req.body,
+                shopId: req.user?.shopId // Assuming shopId is passed in the request body
+            };
+
+            const product = await productService.createProduct(productData);
+
+            res.status(201).json({ message: 'Product added successfully', product });
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).json({ message: error.message });
+            } else {
+                res.status(400).json({ message: 'An unknown error occurred' });
+            }
+        }
+    },
+
+    getAllProducts: async (req: Request, res: Response) => {
+        try {
+            const products = await productService.getAllProducts();
+
+            res.status(200).json(products);
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).json({ message: error.message });
+            } else {
+                res.status(400).json({ message: 'An unknown error occurred' });
+            }
+        }
+    },
+
+    getProduct: async (req: Request, res: Response) => {
+        try {
+            const product = await productService.getProduct(req.params.id);
+
+            res.status(200).json(product);
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).json({ message: error.message });
+            } else {
+                res.status(400).json({ message: 'An unknown error occurred' });
+            }
+        }
+    },
+
+    updateProduct: async (req: CustomRequest, res: Response) => {
+        try {
+            const product = await productService.updateProduct(
+                req.params.id, 
+                req.body, 
+                req.user?.shopId || ''
+            );
+            
+            res.status(200).json(product);
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).json({ message: error.message });
+            } else {
+                res.status(400).json({ message: 'An unknown error occurred' });
+            }
+        }
+    },
+
+    deleteProduct: async (req: CustomRequest, res: Response) => {
+        try {
+            await productService.deleteProduct(
+                req.params.id, 
+                req.user?.shopId || ''
+            );
+
+            res.status(204).send();
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).json({ message: error.message });
+            } else {
+                res.status(400).json({ message: 'An unknown error occurred' });
+            }
+        }
+    },
+};
