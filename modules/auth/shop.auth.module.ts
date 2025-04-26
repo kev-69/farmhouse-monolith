bc5e400a6b595acec97605c1245776c92144aec3
kev-69 <bismarkobuobi19@gmail.com>
2025-04-26 20:38:13 +0000

    feat: add validation middleware for signup and login routes

--- modules/auth/shop.auth.module.ts
+++ modules/auth/shop.auth.module.ts
@@ -6,9 +6,18 @@ import { authController } from './shop.auth.controller'
 const router = Router()
 
 // middlewares
+// middlewares to validate product data
+import { validate } from '../../middlewares/validation.middleware';
+import { signupSchema, loginSchema } from './shop.auth.schema';
 
 // define routes
-router.post('/signup', authController.signup)
-router.post('/login', authController.login)
+router.post('/signup',
+    validate({ body: signupSchema }),
+    authController.signup
+)
+router.post('/login', 
+    validate({ body: loginSchema }),
+    authController.login
+)
 
 export const shopAuthModule = router
