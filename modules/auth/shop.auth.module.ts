ab02408ffc08f3697ef5b7f3bbbed88236cd2bbf
kev-69 <bismarkobuobi19@gmail.com>
2025-04-27 12:19:59 +0000

    chore: remove unused import for authMiddlewares in shop.auth.module.ts

--- modules/auth/shop.auth.module.ts
+++ modules/auth/shop.auth.module.ts
@@ -1,7 +1,6 @@
 // Combines controller + service into a unit
 import { Router } from 'express'
 import { authController } from './shop.auth.controller'
-// import { authMiddlewares } from '../../middlewares/shop.middleware'
 
 const router = Router()
 
