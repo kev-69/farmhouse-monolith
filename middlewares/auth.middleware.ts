51b7ca9ceedf46bdb9057ef462cc628b7712c674
kev-69 <bismarkobuobi19@gmail.com>
2025-04-29 12:33:52 +0000

    feat: add rate limiting middleware to prevent abuse

--- middlewares/auth.middleware.ts
+++ middlewares/auth.middleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import rateLimit from 'express-rate-limit';
 
 // Extend Express Request interface to include 'user' property
 declare global {
@@ -58,3 +59,11 @@ export const authMiddlewares = {
         next();
     },
 };
+
+export const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // Limit each IP to 5 requests per windowMs
+    message: 'Too many requests from this IP, please try again later.',
+    // standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    // legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
