9e55e5f4b6a361944539d7b397603714196dbd49
kev-69 <bismarkobuobi19@gmail.com>
2025-04-30 10:20:33 +0000

    feat: update Prisma client import to shared directory for better modularity

--- modules/categories/category.service.ts
+++ modules/categories/category.service.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-export const prisma = new PrismaClient();
+import { prisma } from '../../shared/prisma';
 
 export const categoryService = {
     createCategory: async (data: any) => {
