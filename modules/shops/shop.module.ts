4a67d80177e9324f5f92ed6eb708aa04b5e42bcd
kev-69 <bismarkobuobi19@gmail.com>
2025-05-04 17:54:12 +0000

    style: improve formatting of shop routes for better readability

--- modules/shops/shop.module.ts
+++ modules/shops/shop.module.ts
@@ -7,8 +7,19 @@ import validateToken from '../../middlewares/user.middleware'
 const router = Router()
 
 // define routes
-router.get('/profile', validateToken, shopController.getShop)
-router.put('/update-profile', validateToken, shopController.updateShop)
-router.delete('/delete-profile', validateToken, shopController.deleteShop)
+router.get('/profile', 
+    validateToken, 
+    shopController.getShop
+)
+
+router.put('/update-profile', 
+    validateToken, 
+    shopController.updateShop
+)
+
+router.delete('/delete-profile', 
+    validateToken, 
+    shopController.deleteShop
+)
 
 export const shopModule = router
