4f9ddeb4a5bb9111abfe6843279092e05125701c
kev-69 <bismarkobuobi19@gmail.com>
2025-05-04 17:54:21 +0000

    style: improve formatting of user routes for better readability

--- modules/users/user.module.ts
+++ modules/users/user.module.ts
@@ -8,12 +8,38 @@ import validateToken from '../../middlewares/user.middleware';
 const router = Router();
 
 // Define routes
-router.get('/profile', validateToken, userController.getUser);
-router.put('/update-profile', validateToken, userController.updateUser);
-router.delete('/delete-profile', validateToken, userController.deleteUser);
-router.post('/profile/add-address', validateToken, userController.addAddress);
-router.put('/profile/update-address/:addressId', validateToken, userController.updateAddress);
-router.get('/addresses', validateToken, userController.getAddresses);
-router.delete('/profile/delete-address/:addressId', validateToken, userController.deleteAddress);
+router.get('/profile', 
+    validateToken, 
+    userController.getUser
+);
+
+router.put('/update-profile', 
+    validateToken, 
+    userController.updateUser
+);
+router.delete('/delete-profile', 
+    validateToken, 
+    userController.deleteUser
+);
+
+router.post('/profile/add-address', 
+    validateToken, 
+    userController.addAddress
+);
+
+router.put('/profile/update-address/:addressId', 
+    validateToken, 
+    userController.updateAddress
+);
+
+router.get('/addresses', 
+    validateToken, 
+    userController.getAddresses
+);
+
+router.delete('/profile/delete-address/:addressId', 
+    validateToken, 
+    userController.deleteAddress
+);
 
 export const userModule = router;
