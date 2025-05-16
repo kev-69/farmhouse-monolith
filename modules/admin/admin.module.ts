e00c6dc399a8731a769fc183ffe5f64245f6e4fa
kev-69 <bismarkobuobi19@gmail.com>
2025-05-16 09:41:52 +0000

    style: create admin module with initial router setup

---
+++ modules/admin/admin.module.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+// import admin middlewares
+
+const router = Router();
+
+// import all routes here
+// login
+
+// get all shops
+
+// get unapproved shops
+
+// approve a shop
+
+// ban a shop
+
+// unban a shop
+
+export const adminModule = router
