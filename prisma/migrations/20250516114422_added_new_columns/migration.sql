0f366cc057e9e68a741fdfc9437bec6c2b1ac261
kev-69 <bismarkobuobi19@gmail.com>
2025-05-16 12:12:50 +0000

    refactor: add isBanned column to Shop table and remove isVerified column from User table

---
+++ prisma/migrations/20250516114422_added_new_columns/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isVerified` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Shop" ADD COLUMN     "isBanned" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "isVerified";
