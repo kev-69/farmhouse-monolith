9b17389ae6c5c0c0edab42d39803950735630e49
kev-69 <bismarkobuobi19@gmail.com>
2025-05-16 11:04:59 +0000

    style: add login and getAllShops routes to admin module

--- modules/admin/admin.module.ts
+++ modules/admin/admin.module.ts
@@ -1,12 +1,23 @@
 import { Router } from "express";
+import { AdminController } from "./admin.controller";
 // import admin middlewares
 
 const router = Router();
 
 // import all routes here
 // login
+router.post("/login",
+    // validate({ body: loginSchema }),
+    // authLimiter,
+    AdminController.login
+);
 
 // get all shops
+router.get("/shops",
+    // authMiddlewares.isAdmin,
+    // validate({ query: getShopsSchema }),
+    AdminController.getAllShops
+);
 
 // get unapproved shops
 
