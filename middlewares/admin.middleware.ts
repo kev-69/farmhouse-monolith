be8546770ca58e335d0047dd4af51e89c3d5187b
kev-69 <bismarkobuobi19@gmail.com>
2025-05-16 09:41:23 +0000

    style: remove admin middleware as part of project restructuring

--- middlewares/admin.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { AppError } from '../utils/errors';
-
-export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
-  if (req.user?.role !== 'ADMIN') {
-    throw new AppError('Unauthorized: Admin access required', 403);
-  }
-  next();
-};
