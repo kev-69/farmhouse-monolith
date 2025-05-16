9c48d1470893a0baf2303ee2b6f1563dcdae7c2e
kev-69 <bismarkobuobi19@gmail.com>
2025-05-16 12:11:46 +0000

    refactor: enhance admin module with user and shop management routes

--- modules/admin/admin.module.ts
+++ modules/admin/admin.module.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
 import { AdminController } from "./admin.controller";
+import { shopController } from "../shops/shop.controller";
+
 // import admin middlewares
+import {
+    validateToken,
+    isAdmin,
+} from "./admin.middleware";
+import { authLimiter } from "../../middlewares/auth.middleware";
 
 const router = Router();
 
@@ -8,23 +15,50 @@ const router = Router();
 // login
 router.post("/login",
     // validate({ body: loginSchema }),
-    // authLimiter,
+    authLimiter,
     AdminController.login
+);
+
+// get all users
+router.get("/users",
+    validateToken,
+    isAdmin,
+    AdminController.getAllUsers
 );
 
 // get all shops
 router.get("/shops",
     // authMiddlewares.isAdmin,
     // validate({ query: getShopsSchema }),
-    AdminController.getAllShops
+    shopController.getAllShops
 );
 
 // get unapproved shops
+router.get("/unapproved-shops",
+    validateToken,
+    isAdmin,
+    AdminController.getUnapprovedShops
+);
 
 // approve a shop
+router.post("/approve-shop/:shopId",
+    validateToken,
+    isAdmin,
+    AdminController.approveShop
+);
 
 // ban a shop
+router.post("/ban-shop/:shopId",
+    validateToken,
+    isAdmin,
+    AdminController.banShop
+);
 
 // unban a shop
+router.post("/unban-shop/:shopId",
+    validateToken,
+    isAdmin,
+    AdminController.unbanShop
+);
 
 export const adminModule = router
