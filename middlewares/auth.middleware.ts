179591cc287cbd2e5d345958764ad02e204df159
kev-69 <bismarkobuobi19@gmail.com>
2025-05-17 16:02:41 +0000

    refactor: increase request limit in auth rate limiter from 5 to 10

--- middlewares/auth.middleware.ts
+++ middlewares/auth.middleware.ts
@@ -62,7 +62,7 @@ export const authMiddlewares = {
 
 export const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, // Limit each IP to 5 requests per windowMs
+    max: 10, // Limit each IP to 5 requests per windowMs
     message: 'Too many requests from this IP, please try again later.',
     // standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     // legacyHeaders: false, // Disable the `X-RateLimit-*` headers
